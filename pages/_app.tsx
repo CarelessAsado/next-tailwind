558bf89c9f4fc9ae95cebf98506507a484ab2849
Rodrigo López <rodrigohernanlopez89@gmail.com>
2023-01-06 11:51:56 -0300

    n

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import "styles/globals.css";
 import type { AppProps } from "next/app";
 import { ApolloProvider } from "@apollo/client";
-import { client } from "store/apollo";
+import { client } from "clientStore/apollo";
 export default function App({ Component, pageProps }: AppProps) {
   return;
   <ApolloProvider client={client}>
