658c8d3dfb2bfe54992d7460219c62b5900dd031
Rodrigo López <rodrigohernanlopez89@gmail.com>
2023-01-10 12:20:18 -0300

    fixed minor bugs on typegoose model + model export

--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -20,10 +20,12 @@ import dbConnect from "utils/dbConnect";
 
 //el _id tecnicamente lo agrega typegoose x default, pero cuando hago un query no me salta
 //le tuve q agregar explicitamente el _id, y, VERY IMPORTANT, add ID en Field
+//10/1/23 Typegoose infiere el _id x default, no hace falta tampoco PONER PROP, pero Typegraphql sí lo necesita
+//ver dsp de dejar solo string, a ver q pasa
 @ObjectType()
-class Task {
+export class Task {
   @Field(() => ID)
-  @prop()
+  /* @prop() sacarle el prop de Typegoose, xq sino cuando trato de generar una nueva Task, tengo q generar el _id yo.*/
   _id!: mongoose.Types.ObjectId;
 
   @Field()
